bd986e5111ea92ce897fe2834d978b803d2a78a8
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-03-11 15:02:49 -0300

    Colocando evento no Botão

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./styles.css";
 
 export default class App extends React.Component {
+  lidarComClick = () => {
+    console.log("deu certo");
+  };
   render() {
-    return <div className="main">olá</div>;
+    return <button onClick={this.lidarComClick}>Meu botão</button>;
   }
 }
