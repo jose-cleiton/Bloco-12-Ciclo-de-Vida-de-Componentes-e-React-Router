51a15c500fb86f641d5dd6ee6ba7fed3cd85c09a
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-03-11 14:57:31 -0300

    Tirando coisas não essenciais

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
+import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="main">
-      <ComponentePai>
-        <p>SUPIMPA</p>
-        <h1>BACANA</h1>
-        <span>INCRÍVEL</span>
-      </ComponentePai>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return <div className="main">olá</div>;
+  }
 }
-const ComponentePai = (props) => {
-  const { children } = props;
-  return (
-    <div>
-      {children}
-      {children}
-    </div>
-  );
-};
